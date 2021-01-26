bfa40a3ae48d6f16801f1627d0534c539a6ff118
ignacio <nachomattos78@gmail.com>
2021-01-26 10:01:52 +0000

    styling images

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -3,7 +3,9 @@ import React from 'react';
 const MovieList = (props) =>{
     return (
         <>
-        {props.movies.map((movie, index)=> <div>
+        {props.movies.map((movie, index)=> 
+        // Adding style to make all the pics same size with a separation btw them
+        <div className="d-flex justify-content-start m-3">
             <img src={movie.Poster} alt='movie'></img>
         </div>)}
         </>
