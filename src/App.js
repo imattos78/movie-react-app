4dc8aecbfdfafa81cce1bc4135cfc1320fbca10f
ignacio <nachomattos78@gmail.com>
2021-01-26 09:50:27 +0000

    adding side scrolling on images

--- src/App.js
+++ src/App.js
@@ -78,10 +78,11 @@ const [movies, setMovies] = useState([
 }
 ])
   return ( 
-  <div className='container-fluid'>
+  <div className='container-fluid movie-app'>
   <div className='row'>
     <MovieList movies={movies} />
-  </div></div> )
+  </div>
+  </div> )
 
 };
 
