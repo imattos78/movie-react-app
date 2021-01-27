f3fdb170f6c492afa1b17d8e9e49b1c9cbab33ac
ignacio <nachomattos78@gmail.com>
2021-01-27 06:31:19 +0000

    favorite bar_bottom images/transition and transform effect

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -5,8 +5,9 @@ const MovieList = (props) =>{
         <>
         {props.movies.map((movie, index)=> 
         // Adding style to make all the pics same size with a separation btw them
-        <div className="d-flex justify-content-start m-3">
+        <div className="image-container d-flex justify-content-start m-3">
             <img src={movie.Poster} alt='movie'></img>
+            <div className="overlay d-flex align-items-center justify-content"></div>
         </div>)}
         </>
     )
